d66559b7ae03dd86357b63989f026bc78d425976
Tefa600 <abdellatifmostafa06@gmail.com>
2022-12-19 06:26:26 +0200

    Home
    added section 1, 2, 3 in the home page, while it's not finished yet, and it apparently needs some enhancements

--- Total-United/src/app/components/home/home.component.ts
+++ Total-United/src/app/components/home/home.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  sec2Inline ="Cras tincidunt tellus at mi tristique rhoncus. Etiam dapibus rutrum leo consectetur accumsan. Vivamus viverra ante turpis, dignissim condimentum elit egestas sit amet. Phasellus faucibus pellentesque";
+  b1Inline="Turnkey projects, Fa code works, Mechanical and electrical ";
+  b2Inline="Sport Flooring, squash court, Padel Courts, Sport equipment";
+  b3Inline="Lorem Ipsum is simply dummy text of the printing";
 
   constructor() { }
 
